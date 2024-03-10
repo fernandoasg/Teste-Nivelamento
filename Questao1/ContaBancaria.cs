a1a63353fc0a0bf44f0b40159b2619ef632faea7
Fernando <fernando.almmm@gmail.com>
2024-03-09 20:57:40 -0400

    Questao 2

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -4,6 +4,38 @@ namespace Questao1
 {
     class ContaBancaria {
 
-       
+        public long NumeroConta { get; }
+
+        public string NomeTitular { get; set; }
+
+        public double Saldo { get; set; }
+
+        public ContaBancaria(long numeroConta, string nomeTitular)
+        {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+        }
+
+        public ContaBancaria(long numeroConta, string nomeTitular, double saldo)
+        {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+            this.Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            this.Saldo -= valor + 3.50;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + this.NumeroConta + ", Titular: " + this.NomeTitular + ", Saldo: $ " + this.Saldo.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+        }
     }
 }
